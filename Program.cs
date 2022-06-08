02a0fd190f955c9cc6d7ba5c0c5eba521e1a05bb
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2022-06-08 16:55:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Atividade
+
+var nomeDoUsuario = "João";
+var idadeDoUsuario = 20;
+var anoAtual = 2022;
+
+var anoNascimento = anoAtual - idadeDoUsuario;
+
+
+Console.WriteLine($"Olá {nomeDoUsuario}, você nasceu em {anoNascimento}");
+
+//Desafio
+
+var quilometrosPercorridosPeloVeiculo = 80000;
+var milhasPorKm = 0.6214;
+var milhasPercorridasPeloCarro = quilometrosPercorridosPeloVeiculo * milhasPorKm;
+Console.WriteLine($"Seu veículo percorreu um total de {milhasPercorridasPeloCarro} milhas.");
+
+
+
