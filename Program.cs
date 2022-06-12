8422097359769bb6a07c56a8eb077787ba698c43
Sabrina Helena Ferreira <101990175+sabrinahelena@users.noreply.github.com>
2022-06-12 18:28:58 -0300

    Atualizei o código da atividade
    Adicionei a inserção e leitura de dados.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿//Atividade
 
-var nomeDoUsuario = "João";
-var idadeDoUsuario = 20;
-var anoAtual = 2022;
+string nomeDoUsuario;
+int idadeDoUsuario;
+int anoAtual = 2022;
+int calcularAnoNascimento;
 
-var anoNascimento = anoAtual - idadeDoUsuario;
 
+Console.WriteLine("Digite o seu nome:");
+nomeDoUsuario = Console.ReadLine();
 
-Console.WriteLine($"Olá {nomeDoUsuario}, você nasceu em {anoNascimento}");
+Console.WriteLine($"Olá, {nomeDoUsuario}! Seja bem-vindo ao cálculo do seu ano de nascimento!");
+
+Console.WriteLine("Agora, digite a sua idade para prosseguirmos:");
+idadeDoUsuario = Convert.ToInt32(Console.ReadLine()); //o ReadLine quando usado para capturar números você deve realizar a conversão.
+
+calcularAnoNascimento = anoAtual
+    - idadeDoUsuario; //cálculo básico para determinar ano de nascimento, subtraindo o ano atual pela idade do usuário.
+
+Console.WriteLine($"Você nasceu em: {calcularAnoNascimento}");
 
 //Desafio
 
